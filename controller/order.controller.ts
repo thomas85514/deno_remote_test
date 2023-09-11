0390b932d8f103bb291ba326966814a5a200ccef
he8549 <thomas.fan@hepiuscare.com.tw>
2023-09-11 14:08:31 +0800

    try reload raw

--- controller/order.controller.ts
+++ controller/order.controller.ts
@@ -1,12 +1,15 @@
 // deno-lint-ignore-file no-explicit-any
-// import { OrderService } from '@his/model/order-service/mod.ts';
-import { Controller, Subscriber, Replier } from 'https://raw.githubusercontent.com/thomas85514/deno_remote_test/main/controller/mod.ts';
+// import { OrderService } from '@his/model/order-service/mod.ts';a
+import {
+  Controller,
+  Subscriber,
+  Replier,
+} from 'https://raw.githubusercontent.com/thomas85514/deno_remote_test/main/controller/mod.ts';
 import { JsMsg, Msg, Codec } from 'https://deno.land/x/nats@v1.16.0/src/mod.ts';
 
 @Controller('order')
 export class OrderController {
-  constructor() // private readonly orderService: OrderService = new OrderService(),
-  {}
+  constructor() {} // private readonly orderService: OrderService = new OrderService(),
 
   @Subscriber('insert')
   async insertOrder(message: JsMsg, payload: any) {
