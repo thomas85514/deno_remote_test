20e6f258d8b9b4838cf938b87cadc6264ef3bb79
thomas85514 <141607485+thomas85514@users.noreply.github.com>
2023-09-11 13:59:21 +0800

    Create order.controller.ts

---
+++ controller/order.controller.ts
@@ -0,0 +1,31 @@
+// deno-lint-ignore-file no-explicit-any
+// import { OrderService } from '@his/model/order-service/mod.ts';
+import { Controller, Subscriber, Replier } from '@his/base/server-base/mod.ts';
+import { JsMsg, Msg, Codec } from 'https://deno.land/x/nats@v1.16.0/src/mod.ts';
+
+@Controller('order')
+export class OrderController {
+  constructor() // private readonly orderService: OrderService = new OrderService(),
+  {}
+
+  @Subscriber('insert')
+  async insertOrder(message: JsMsg, payload: any) {
+    try {
+      console.log('Processing insertOrder: ', payload);
+      // await this.orderService.insertOrder();
+
+      message.ack();
+    } catch (error) {
+      console.error('Error while getOrders: ', error);
+      message.nak();
+    }
+  }
+
+  @Replier('list')
+  async getOrders(message: Msg, payload: any, jsonCodec: Codec<any>) {
+    console.log('Processing getOrders: ', payload);
+    // const orders = await this.orderService.getAllOrders();
+
+    message.respond(jsonCodec.encode([]));
+  }
+}
