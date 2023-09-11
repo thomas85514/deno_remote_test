203aab21065e4fc666b4b129b42eee673f0c56d9
thomas85514 <141607485+thomas85514@users.noreply.github.com>
2023-09-11 14:00:01 +0800

    Create mod.ts

---
+++ base/mod.ts
@@ -0,0 +1,5 @@
+export * from './controller.service.ts';
+export * from './decorators.ts';
+export * from './instanceProvider.ts';
+export * from './server.interface.ts';
+export * from './utils.ts';
